c837ee14c11b377bf089b7158b5d41f325289a58
mecha2k@naver.com <mecha2k@naver.com>
2024-10-03 12:41:03 +0900

    init

--- transformers/config.py
+++ transformers/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 def get_config():
     return {
-        "batch_size": 32,
+        "batch_size": 8,
         "num_epochs": 2,
         "lr": 10**-4,
         "seq_len": 350,
--- transformers/train.py
+++ transformers/train.py
@@ -23,9 +23,7 @@ import torchmetrics
 from torch.utils.tensorboard import SummaryWriter
 
 
-def greedy_decode(
-    model, source, source_mask, tokenizer_src, tokenizer_tgt, max_len, device
-):
+def greedy_decode(model, source, source_mask, tokenizer_src, tokenizer_tgt, max_len, device):
     sos_idx = tokenizer_tgt.token_to_id("[SOS]")
     eos_idx = tokenizer_tgt.token_to_id("[EOS]")
 
@@ -38,9 +36,7 @@ def greedy_decode(
             break
 
         # build mask for target
-        decoder_mask = (
-            causal_mask(decoder_input.size(1)).type_as(source_mask).to(device)
-        )
+        decoder_mask = causal_mask(decoder_input.size(1)).type_as(source_mask).to(device)
 
         # calculate output
         out = model.decode(encoder_output, source_mask, decoder_input, decoder_mask)
@@ -208,9 +204,7 @@ def get_ds(config):
     print(f"Max length of source sentence: {max_len_src}")
     print(f"Max length of target sentence: {max_len_tgt}")
 
-    train_dataloader = DataLoader(
-        train_ds, batch_size=config["batch_size"], shuffle=True
-    )
+    train_dataloader = DataLoader(train_ds, batch_size=config["batch_size"], shuffle=True)
     val_dataloader = DataLoader(val_ds, batch_size=1, shuffle=True)
 
     return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
@@ -253,14 +247,12 @@ def train_model(config):
     device = torch.device(device)
 
     # Make sure the weights folder exists
-    Path(f"{config['datasource']}_{config['model_folder']}").mkdir(
-        parents=True, exist_ok=True
-    )
+    Path(f"{config['datasource']}_{config['model_folder']}").mkdir(parents=True, exist_ok=True)
 
     train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt = get_ds(config)
-    model = get_model(
-        config, tokenizer_src.get_vocab_size(), tokenizer_tgt.get_vocab_size()
-    ).to(device)
+    model = get_model(config, tokenizer_src.get_vocab_size(), tokenizer_tgt.get_vocab_size()).to(
+        device
+    )
     # Tensorboard
     writer = SummaryWriter(config["experiment_name"])
 
@@ -277,7 +269,7 @@ def train_model(config):
     )
     if model_filename:
         print(f"Preloading model {model_filename}")
-        state = torch.load(model_filename)
+        state = torch.load(model_filename, map_location=torch.device("cpu"))
         model.load_state_dict(state["model_state_dict"])
         initial_epoch = state["epoch"] + 1
         optimizer.load_state_dict(state["optimizer_state_dict"])
@@ -301,9 +293,7 @@ def train_model(config):
             decoder_mask = batch["decoder_mask"].to(device)  # (B, 1, seq_len, seq_len)
 
             # Run the tensors through the encoder, decoder and the projection layer
-            encoder_output = model.encode(
-                encoder_input, encoder_mask
-            )  # (B, seq_len, d_model)
+            encoder_output = model.encode(encoder_input, encoder_mask)  # (B, seq_len, d_model)
             decoder_output = model.decode(
                 encoder_output, encoder_mask, decoder_input, decoder_mask
             )  # (B, seq_len, d_model)
@@ -313,9 +303,7 @@ def train_model(config):
             label = batch["label"].to(device)  # (B, seq_len)
 
             # Compute the loss using a simple cross entropy
-            loss = loss_fn(
-                proj_output.view(-1, tokenizer_tgt.get_vocab_size()), label.view(-1)
-            )
+            loss = loss_fn(proj_output.view(-1, tokenizer_tgt.get_vocab_size()), label.view(-1))
             batch_iterator.set_postfix({"loss": f"{loss.item():6.3f}"})
 
             # Log the loss
